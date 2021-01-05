f6a72832d908d29cd36f91a0e11a581181f29215
Kornelyzas <kornelgaiz@gmail.com>
2021-01-05 20:18:14 +0200

    a commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="home-route"><b>HOME</b></div>
+            <div className="work-route"><b>CV</b></div>
+            <div className="work-route"><b>WORK</b></div>
+        </div>
+    )
+}
+
+export default Header
