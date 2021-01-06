50f9abe3289e23f2940d26af0710a5e442140e19
Kornelyzas <kornelgaiz@gmail.com>
2021-01-06 21:45:43 +0200

    01-06

--- src/components/cv/Cv.jsx
+++ src/components/cv/Cv.jsx
@@ -14,8 +14,24 @@ function Cv() {
             </div>
             <div className="cv-main">
                 <div className="left-side">
-
-                </div>
+                    <div className="photo"></div>
+                        <div className="lang-skills">
+                            <h3>Language Skills</h3>
+                            <p className="p-div">
+                                <b className="p-box">English</b> B2
+                                <b className="p-box">Russian</b> B1
+                                <b className="p-box">Dutch</b> A1
+                            </p>
+                        </div>
+                        <div className="about-me">
+                            <h3>About Me</h3>
+                            <p>
+                                "Hi! I'm a begginer Web Developer, I'm studying in Kaunas Information
+                                Technology School. At the moment I'm finished Front-End course,
+                                but studying more by myself. And waiting with excitment for Back-End!"
+                            </p>
+                        </div>
+                    </div>
                 <div className="middle-side">
                     <div className="work-xp">
                         <h3 className="xp-div">Work Experience</h3>
